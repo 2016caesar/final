01129ef0a2d4248165a7ed99e40a7923fcd7899c
jkw15g <jkw15g@gmail.com>
2016-08-15 18:45:29 +0900

    Main import 수정

--- src/finals/comm/MainAction.java
+++ src/finals/comm/MainAction.java
@@ -1,10 +1,6 @@
 package finals.comm;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Map;
-import java.util.TimeZone;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -12,11 +8,6 @@ import javax.servlet.http.HttpServletResponse;
 import org.apache.struts.action.ActionForm;
 import org.apache.struts.action.ActionForward;
 import org.apache.struts.action.ActionMapping;
-import org.apache.commons.codec.binary.Base64;
-
-import finals.util.AES128;
-import finals.util.HmacSha256;
-import finals.util.SendMail;
 public class MainAction extends CommonAction{
 
 	public ActionForward doExecute(
@@ -32,7 +23,7 @@ public class MainAction extends CommonAction{
 
 			// Seed for HMAC-SHA256 - 32 bytes
 	      
-			MainDao dao = new MainDao();
+			MainDao dao = new MainDao(); 
 
 		}catch(Exception e){
 			logger.error(e);
